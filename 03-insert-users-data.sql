e95af4d5d8c89d9857160d259e4a5acd0075305d
Isaac Sokari <isaacsokari@gmail.com>
2022-05-02 23:04:04 +0100

    feat: insert users data

---
+++ 03-insert-users-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES(20000, 'John Doe', 'employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES(0, 'Unemployed Doe', 'unemployed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES(15000, 'SelfEmployed Doe', 'self-employed');
