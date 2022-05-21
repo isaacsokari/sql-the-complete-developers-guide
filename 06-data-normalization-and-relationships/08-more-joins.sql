4378282ddfe1f3d3eceb775bf56ae0bb921a1e5d
Isaac Sokari <isaacsokari@gmail.com>
2022-05-21 22:22:26 +0100

    an example using joins

---
+++ 06-data-normalization-and-relationships/08-more-joins.sql
@@ -0,0 +1,6 @@
+-- Goal: to fetch all cities and the users in them
+-- also include cities w/o users
+SELECT c.name AS city_name, u.first_name, u.last_name
+FROM cities AS c
+LEFT JOIN addresses AS a ON c.id = a.city_id
+LEFT JOIN users AS u ON u.address_id = a.id;
