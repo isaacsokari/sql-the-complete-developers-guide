853b22ea4137ebe21e074b28bf863deb14bea605
Isaac Sokari <isaacsokari@gmail.com>
2022-05-22 02:25:19 +0100

    drop tables and create new tables with the relationships

---
+++ 06-data-normalization-and-relationships/20-more-relations.sql
@@ -0,0 +1,28 @@
+CREATE TABLE company_buildings (
+  id SERIAL PRIMARY KEY, -- postgres
+  name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE teams (
+  id SERIAL PRIMARY KEY, -- postgres
+  name VARCHAR(300) NOT NULL,
+  building_id INT REFERENCES company_buildings ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- mysql
+  id SERIAL PRIMARY KEY, -- postgres
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  birthdate DATE NOT NULL,
+  -- email VARCHAR(200) REFERENCES intranet_accounts (email) ON DELETE SET NULL -- can be omitted as email is the primary key
+  email VARCHAR(200) UNIQUE NOT NULL,
+  team_id INT DEFAULT 1 REFERENCES teams ON DELETE SET DEFAULT
+);
+
+CREATE TABLE intranet_accounts (
+  -- id INT PRIMARY KEY AUTO_INCREMENT, -- mysql
+  id SERIAL PRIMARY KEY, -- postgres
+  email VARCHAR(200) REFERENCES employees (email) ON DELETE CASCADE, -- postgres
+  password VARCHAR(200) NOT NULL
+);
