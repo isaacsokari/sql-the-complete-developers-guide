c2fb7ddead78f1372c4fe7f87107969f1a21a416
Isaac Sokari <isaacsokari@gmail.com>
2022-05-22 14:05:51 +0100

    count

---
+++ 08-grouping-and-aggregate-functions/01-count.sql
@@ -0,0 +1,8 @@
+-- count all rows
+-- SELECT COUNT (*) FROM bookings;
+
+-- count all `booking_date`s
+-- SELECT COUNT (booking_date) FROM bookings;
+
+-- count all tips
+SELECT COUNT (amount_tipped) FROM bookings;
