2cc13e09fdacca2d046fcd38c835060de4ad1601
Isaac Sokari <isaacsokari@gmail.com>
2022-05-27 23:17:44 +0100

    learnt to use EXISTS in subqueries

--- 09-builtin-query-functions-and-operators/07-exists-subquery-expressions.sql
+++ 09-builtin-query-functions-and-operators/07-exists-subquery-expressions.sql
@@ -5,8 +5,17 @@
 -- to check if a field exists
 -- it returns a boolean or 1 or 0 
 -- it is more efficient as it stops once a value is found
-SELECT EXISTS(
-  SELECT first_name, last_name
-  FROM customers
-  WHERE email = 'max@test.com'
+-- SELECT EXISTS(
+--   SELECT first_name, last_name
+--   FROM customers
+--   WHERE email = 'max@test.com'
+-- );
+
+-- find the order id of a customer with an email given
+SELECT o.id
+FROM orders AS o
+WHERE EXISTS(
+  SELECT c.email
+  FROM customers AS c
+  WHERE o.customer_id = c.id AND c.email = 'manu@test.com'
 );
